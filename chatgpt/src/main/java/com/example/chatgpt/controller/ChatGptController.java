1571e0827eb9e12c24bbf3c2408a12fb9cdefd8c
vargas <guilhermeinfvargas@gmail.com>
2023-03-04 15:20:01 -0300

    [feat] creating a chat with chatgpt api

---
+++ chatgpt/src/main/java/com/example/chatgpt/controller/ChatGptController.java
@@ -0,0 +1,23 @@
+package com.example.chatgpt.controller;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ChatGptController {
+
+    @PostMapping("/chat")
+    @ResponseBody
+    public ResponseEntity<String> chat(@RequestParam("message") String message) {
+        OpenAiService service = new OpenAiService("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");
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt(message)
+                .model("text-davinci-003")
+                .maxTokens(200)
+                .temperature(0.2)
+                .build();
+        return ResponseEntity.ok(service.createCompletion(completionRequest).getChoices().toString());
+    }
+}
