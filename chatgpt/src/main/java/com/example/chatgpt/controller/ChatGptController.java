822793af30bf7f910a5e9802356b50333e1245a6
Guilherme Vargas <56303179+gvargasx@users.noreply.github.com>
2023-03-04 15:21:39 -0300

    Update ChatGptController.java

--- chatgpt/src/main/java/com/example/chatgpt/controller/ChatGptController.java
+++ chatgpt/src/main/java/com/example/chatgpt/controller/ChatGptController.java
@@ -11,7 +11,7 @@ public class ChatGptController {
     @PostMapping("/chat")
     @ResponseBody
     public ResponseEntity<String> chat(@RequestParam("message") String message) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("your-key-here");
         CompletionRequest completionRequest = CompletionRequest.builder()
                 .prompt(message)
                 .model("text-davinci-003")
